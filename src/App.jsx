04f34f8077e41c0d1ad2a4bb86cd063296057e6d
brightsidearab-a11y <brightsidearab@gmail.com>
2026-02-10 09:44:59 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const App = () => (
+  <div className="landing-page">
+    <h1 style={{color: 'white'}}>Paradise Nursery</h1>
+    <button style={{padding: '10px 20px', cursor: 'pointer'}}>Get Started</button>
+  </div>
+);
+export default App;
